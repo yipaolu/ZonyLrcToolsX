14812bceb72d516430595029f5193c600b490a5f
real-zony <zony@myzony.com>
2024-06-28 12:29:31 +0800

    refactor: Removed the local server project.

--- src/ZonyLrcTools.LocalServer/Contract/Dtos/PagedListRequestDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ZonyLrcTools.LocalServer.Contract.Dtos;
-
-public class PagedListRequestDto
-{
-    public int PageIndex { get; set; }
-    public int PageSize { get; set; }
-}
--- src/ZonyLrcTools.LocalServer/Services/MusicInfo/IMusicInfoService.cs
+++
@@ -1,9 +0,0 @@
-﻿using ZonyLrcTools.LocalServer.Contract.Dtos;
-using ZonyLrcTools.LocalServer.Services.MusicInfo.Dtos;
-
-namespace ZonyLrcTools.LocalServer.Services.MusicInfo;
-
-public interface IMusicInfoService
-{
-    Task<PagedListResultDto<MusicInfoListItemDto>> GetMusicInfoListAsync(MusicInfoListInput input);
-}
